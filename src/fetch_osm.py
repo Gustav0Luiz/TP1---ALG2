d5a7c941119d9c3cf998458bb6b87cd2ae1957f4
Pedro Cortez <108164511+PHMGC@users.noreply.github.com>
2026-04-30 10:09:17 -0300

    feat: fetch refactor (still some missings)

--- src/fetch_osm.py
+++ src/fetch_osm.py
@@ -12,33 +12,70 @@ def get_addresses():
     with open(ADDRESS_PATH, "r", encoding='utf-8') as infile:
         reader = csv.reader(infile)
         next(reader)  # Skip header
-        return [", ".join(row[1:]) for row in reader]
+        names = []
+        addresses = []
+        for row in reader:
+            names.append(row[0])
+            address = {
+                "street": f"{row[1]}, {row[2]}",
+                "city": "Belo Horizonte",
+                "state": "Minas Gerais",
+                "country": "Brazil",
+                "postalcode": row[5]
+            }
+            addresses.append(address)
+        return names, addresses
+    
+def struct_osm(result):
+    if not result:
+        return None
+    address = result.raw.get("address", {})
+    return {
+        "name": address.get("name", ""),
+        "street": address.get("road", ""),
+        "number": address.get("house_number", ""),
+        "district": address.get('suburb') or address.get('neighbourhood', ''),
+        "city_state": f"{address.get('city') or address.get('town', '')} - {address.get('state')}",
+        "zip_code": address.get("postcode"),
+        "latitude": result.raw.get("lat", ''),
+        "longitude": result.raw.get("lon", '')
+    }
 
 def fetch_osm(addresses):
     geolocator = Nominatim(user_agent="TP1-ALG2")
     results = []
     for address in addresses:
-        try:
-            result = geolocator.geocode(address, timeout=10)
-            results.append(result)
-            if result:
-                print(f"Geocoded '{address}' -> {result.latitude}, {result.longitude}: {result.address}")
-            else:
-                print(f"Address '{address}' not found")
-        except Exception as e:
-            print(f"Failed to geocode '{address}': {e}")
-            results.append(None)
-        time.sleep(1)  # Nominatim max usage -> 1 req/sec
+        result = geolocator.geocode(address, addressdetails=True, timeout=10)
+        s_result = struct_osm(result)
+        results.append(s_result)
+        if s_result:
+            print(f"Geocoded '{address}' -> {s_result['latitude']}, {s_result['longitude']}")
+        else:
+            print(f"Address '{address}' not found")
+        time.sleep(0.3)  # Nominatim max usage -> 1 req/sec
     return results
 
-with open(COORDS_PATH, "w", encoding='utf-8') as outfile:
-    writer = csv.writer(outfile)
-    header = ["name", "street", "number", "district", "city_state", "zip_code", "latitude", "longitude"]
-    writer.writerow(header)
-    results = fetch_osm(get_addresses())
-    for i, result in enumerate(results):
-        if result:
-            address = result.address.split(",")[:-1]
-            writer.writerow(address + [result.latitude, result.longitude])
-        else:
-            writer.writerow([None] * len(header))
+def main():
+    names, addresses = get_addresses()
+    results = fetch_osm(addresses)
+
+    with open(COORDS_PATH, "w", encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        header = ["name", "street", "number", "district", "city_state", "zip_code", "latitude", "longitude"]
+        writer.writerow(header)
+        for name, result in zip(names, results):
+            if result:
+                writer.writerow([name,
+                                 result["street"],
+                                 result["number"],
+                                 result["district"],
+                                 result["city_state"],
+                                 result["zip_code"],
+                                 result["latitude"],
+                                 result["longitude"]]
+                                )
+            # else:
+            #     writer.writerow([name] + [None] * (len(header) - 1))
+                
+if __name__ == "__main__":
+    main()
